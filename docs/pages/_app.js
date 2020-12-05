83d2ceacfbc255451d8f41960b9353031d1f35f3
Gheorghe Pinzaru <feross95@gmail.com>
2020-12-05 17:40:45 +0300

    Fix docs deployment

--- docs/pages/_app.js
+++ docs/pages/_app.js
@@ -1,6 +1,8 @@
-import 'nextra-theme-docs/style.css'
-import './table.css'
+import React from 'react';
+
+import 'nextra-theme-docs/style.css';
+import './table.css';
 
 export default function Nextra({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
--- docs/theme.config.js
+++ docs/theme.config.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export default {
   github: 'https://github.com/pacific-dev/seabass',
   titleSuffix: ' – Seabass',
@@ -5,7 +7,7 @@ export default {
     <>
       <span className="mr-2 font-extrabold hidden md:inline">Seabass</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-         React Native primitive UI components. 
+        React Native primitive UI components.
       </span>
     </>
   ),
@@ -15,11 +17,20 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Seabass: React Native primitive UI components." />
-      <meta name="og:description" content="Seabass: React Native primitive UI components." />
+      <meta
+        name="description"
+        content="Seabass: React Native primitive UI components."
+      />
+      <meta
+        name="og:description"
+        content="Seabass: React Native primitive UI components."
+      />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@shuding_" />
-      <meta name="og:title" content="Seabass: React Native primitive UI components." />
+      <meta
+        name="og:title"
+        content="Seabass: React Native primitive UI components."
+      />
       <meta name="apple-mobile-web-app-title" content="Seabass" />
     </>
   ),
@@ -28,5 +39,5 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditOnGitHubLink: true,
-  footerText: <> EPL-2.0 {new Date().getFullYear()} © Pacific Dev.</>
-}
+  footerText: <> EPL-2.0 {new Date().getFullYear()} © Pacific Dev.</>,
+};
